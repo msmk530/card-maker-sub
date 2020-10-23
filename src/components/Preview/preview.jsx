77cf0eec04ac4f4e73e488fa92833dcf840b1ba7
kimminsub <msmk530@naver.com>
2020-10-23 23:42:31 +0900

    [feat] preview card 레이아웃
    더미데이터를 사용하여 preview에 보일 카드의 레이아웃을 마크업하였다.

--- src/components/Preview/preview.jsx
+++ src/components/Preview/preview.jsx
@@ -7,9 +7,11 @@ const Preview = ({ cards }) => {
   return (
     <section className={styles.preview}>
       <h1 className={styles.title}>Card Preview</h1>
-      {cards.map((card) => (
-        <Card card={card} />
-      ))}
+      <ul className={styles.cards}>
+        {cards.map((card) => (
+          <Card card={card} />
+        ))}
+      </ul>
     </section>
   );
 };
