ed6fea650420d95507a3c1c87582a7d819460d0d
msmk530 <msmk530@naver.com>
2020-10-24 14:04:59 +0900

    [modify] edit form 마크업
    edit form의 기본 레이아웃을 마크업 하였다.

--- src/components/CardEditForm/cardEditForm.jsx
+++ src/components/CardEditForm/cardEditForm.jsx
@@ -19,18 +19,25 @@ const CardEditForm = ({ card }) => {
   const onSubmit = () => {};
 
   return (
-    <form>
-      <input type='text' name='name' value={name} />
-      <input type='text' name='company' value={company} />
-      <select name='theme' value={theme}>
+    <form className={styles.form}>
+      <input className={styles.input} type='text' name='name' value={name} />
+      <input
+        className={styles.input}
+        type='text'
+        name='company'
+        value={company}
+      />
+      <select className={styles.select} name='theme' value={theme}>
         <option value='light'>Light</option>
         <option value='dark'>Dark</option>
         <option value='colorful'>Colorful</option>
       </select>
-      <input type='text' name='title' value={title} />
-      <input type='text' name='email' value={email} />
-      <textarea name='message' value={message} />
-      <ImageFileInput />
+      <input className={styles.input} type='text' name='title' value={title} />
+      <input className={styles.input} type='text' name='email' value={email} />
+      <textarea className={styles.textarea} name='message' value={message} />
+      <div className={styles.fileInput}>
+        <ImageFileInput name={name} onClick={onSubmit} />
+      </div>
       <Button name='Delete' onClick={onSubmit} />
     </form>
   );
--- src/components/ImageFileInput/imageFileInput.jsx
+++ src/components/ImageFileInput/imageFileInput.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import styles from './styles/imageFileInput.module.css';
 
-const ImageFileInput = (props) => <button>Image</button>;
+const ImageFileInput = ({ name, onClick }) => (
+  <button className={styles.fileInput} onClick={onClick}>
+    {name}
+  </button>
+);
 
 export default ImageFileInput;
